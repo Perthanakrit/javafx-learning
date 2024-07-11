4f43d793b3a603d71d780b227bb8517ee98673e2
Perthanakrit <aupper094@gmail.com>
2024-07-11 16:56:11 +0700

    Start project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module ku.cs.basicjavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ku.cs.basicjavafx to javafx.fxml;
+    exports ku.cs.basicjavafx;
+
+    exports ku.cs.controllers;
+    opens ku.cs.controllers to javafx.fxml;
+
+    exports ku.cs.services;
+    opens ku.cs.services;
+}
